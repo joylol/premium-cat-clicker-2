c9f1f3ae20865e19f911fb63fc99793624a5c357
Joy <joylol@users.noreply.github.com>
2015-09-04 10:41:21 -0500

    Set up  index.html, style.css, and main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+var model = [
+	{
+		name: 'KitKat',
+		imageSRC: 'cat.jpg',
+		clickCount: 0
+	},
+	{
+		name: 'FunKat',
+		imageSRC: 'cat2.jpg',
+		clickCount: 0;
+	},
+	{
+		name: 'BooKat',
+		imageSRC: 'cat3.jpg',
+		clickCount: 0;
+	},
+	{
+		name: 'LolKat', 
+		imageSRC: 'cat4.jpg',
+		clickCount: 0;
+	},
+	{
+		name: 'FoxKat',
+		imageSRC: 'cat5.jpg',
+		clickCount: 0;
+	}
+]
+
+var octopus = {
+
+};
+
+var listView = {
+
+};
+
+var imageView = {
+
+};
